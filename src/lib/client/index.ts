aaaf7cc2584c28c7569574fe70cae91f8fafa4cc
YUapisit <aps.apisit250@gmail.com>
2026-03-22 22:21:41 +0700

    feat: add OpenAPI schema and client setup for categories, projects, and users

---
+++ src/lib/client/index.ts
@@ -0,0 +1,11 @@
+import createFetchClient from 'openapi-fetch'
+import createClient from 'openapi-react-query'
+import type { paths } from '@/lib/client/api/v1'
+
+const fetchClient = createFetchClient<paths>({
+  baseUrl: '/api',
+})
+
+const $api = createClient(fetchClient)
+
+export { $api }
