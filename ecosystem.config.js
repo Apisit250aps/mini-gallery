4f74eb62ea72f5a247b3cdc480e133ec0aa9a95b
YUapisit <aps.apisit250@gmail.com>
2026-03-24 08:06:43 +0700

    feat: add ecosystem configuration for production deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'next-standalone-app',
+      script: './.next/standalone/server.js',
+      // instances: "max",
+      exec_mode: 'cluster', // ใช้ Cluster Mode เพื่อกระจายโหลด
+      max_memory_restart: '1G', // สั่ง restart ถ้า RAM พุ่งเกิน 1GB
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001,
+        HOSTNAME: '0.0.0.0',
+      },
+    },
+  ],
+}
